7b4262c1bd5a30d1ab27e4a7c2919cd5a26337b4
marshennikovaolga <marshennikova@gmail.com>
2024-11-25 15:34:34 +0100

    .

--- web/app/routes/cv.tsx
+++ web/app/routes/cv.tsx
@@ -1,46 +1,33 @@
 import * as React from "react"
 import { createFileRoute } from "@tanstack/react-router"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {
-  faInstagram,
-  faGithub,
-  faTwitter,
-  faSpotify,
-  faDiscord,
-  faGoogle,
-} from "@fortawesome/free-brands-svg-icons"
+import { Link as LinkIcon } from "lucide-react"
 
 export const Route = createFileRoute("/cv")({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  const links = [
-    { icon: faInstagram, href: "https://www.instagram.com/olyamrshn" },
-    { icon: faGithub, href: "https://github.com/olyamrshn" },
-    { icon: faTwitter, href: "https://twitter.com/leatbabies" },
-    {
-      icon: faSpotify,
-      href: "https://open.spotify.com/user/awpkpo5yx2t80iqa4tgfvp7a7",
-    },
-    { icon: faDiscord, href: "https://discord.com/users/704485765010161966" },
-    { icon: faGoogle, href: "mailto:olyamrshn@gmail.com" },
-  ]
-
   const experience = [
     {
-      title: "Learn Anything | Frontend Dev (2023 - Present)",
-      points: [
-        "frontend development for an open-source educational platform featuring thousands of curated learning topics",
-        "Implemented core features including user notes, task management, and advanced search functionality",
+      title: "Learn Anything (2023 - Present)",
+      description: [
+        "An open-source educational platform featuring thousands of curated learning topics",
       ],
+      link: "https://github.com/learn-anything/learn-anything",
     },
     {
-      title: "Zuleika Jewelry | Frontend Dev (2024 - Present)",
-      points: [
-        "Developed and launched a comprehensive e-commerce interface for a jewelry store on Shopify platform",
-        "Implemented responsive product galleries, collection displays, and detailed product cards",
+      title: "Zuleika Jewelry (2024 - Present)",
+      description: [
+        "A comprehensive e-commerce solution including custom interface, product management, and inventory tracking for a jewelry store on Shopify platform",
       ],
+      link: "https://github.com/olyamrshn/zuleika-jewelry",
+    },
+    {
+      title: "The Pact (2024)",
+      description: [
+        "Protocol for creating and managing escrow contracts for task-based funding",
+      ],
+      link: "https://github.com/thepakt/app.thepakt.com",
     },
   ]
 
@@ -48,6 +35,7 @@ function RouteComponent() {
     {
       title: "Winner, TON Paris Hackathon (October 2024)",
       subtitle: "Frontend Developer - TON Todo Escrow Application",
+      link: "https://x.com/ton_france/status/1851538025810952193",
       description:
         "frontend development of a Telegram mini-app connecting entrepreneurs with investors via a task-based escrow system. A secure platform allowing entrepreneurs to structure projects into funded tasks while protecting investor assets through blockchain-based escrow functionality.",
       points: [
@@ -61,14 +49,14 @@ function RouteComponent() {
   const additionalEducation = [
     {
       title:
-        "Yandex Practicum | Frontend Full Development Course - 2022 (1 year)",
+        "- Yandex Practicum | Frontend Full Development Course - 2022 (1 year)",
       points: [
         "Frontend development fundamentals",
         "React and modern web technologies",
       ],
     },
     {
-      title: "Animations.dev",
+      title: "- Animations.dev",
       points: [
         "Advanced web animations",
         "CSS and JavaScript animation techniques",
@@ -80,7 +68,6 @@ function RouteComponent() {
     const skills = {
       "Core Technologies": ["TypeScript", "REST API", "Three.js"],
       "Frameworks & Libraries": [
-        "React.js",
         "Next.js",
         "React Native",
         "TailwindCSS",
@@ -101,9 +88,9 @@ function RouteComponent() {
               {items.map((skill) => (
                 <span
                   key={skill}
-                  className="text-sm px-4 py-2 bg-gradient-to-r from-purple-500/20 to-blue-500/20
-                    border border-purple-500/30 rounded-lg transition-all duration-300
-                    hover:from-purple-500/30 hover:to-blue-500/30 hover:border-purple-500/50
+                  className="text-sm px-4 py-2 bg-gradient-to-r from-neutral-800/20 to-neutral-800/20
+                    border border-neutral-800/30 rounded-lg transition-all duration-300
+                    hover:from-neutral-800/30 hover:to-neutral-800/30 hover:bg-neutral-800
                     hover:scale-105 cursor-default"
                 >
                   {skill}
@@ -137,11 +124,24 @@ function RouteComponent() {
       subtitle?: string
       description?: string
       points?: string[]
+      link?: string
     }
   }) => (
     <div className="mb-6">
       <h3 className="text-sm font-regular whitespace-normal text-left">
-        {item.title}
+        {item.link ? (
+          <a
+            href={item.link}
+            className="hover:text-white/50 text-white transition-opacity duration-300 flex items-center gap-2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkIcon className="w-4 h-4" />
+            {item.title}
+          </a>
+        ) : (
+          item.title
+        )}
       </h3>
       {item.subtitle && (
         <p className="text-sm opacity-70 whitespace-normal text-left">
@@ -149,30 +149,28 @@ function RouteComponent() {
         </p>
       )}
       {item.description && (
-        <p className="text-sm mt-2 whitespace-normal text-left">
+        <p className="text-sm mt-2 opacity-70 whitespace-normal text-left">
           {item.description}
         </p>
-      )}
-      {item.points && (
-        <ul className="text-sm list-disc pl-4 mt-2 opacity-70">
-          {item.points.map((point, index) => (
-            <li key={index} className="whitespace-normal mb-2 text-left">
-              {point}
-            </li>
-          ))}
-        </ul>
       )}
     </div>
   )
 
   return (
-    <div className="text-white mx-auto my-4 max-w-3xl user-select-none">
+    <div className="text-white mx-auto my-4 max-w-2xl user-select-none">
       <div className="text-white mx-auto max-w-screen-md px-4 text-left">
         <h1 className="text-base font-regular mb-3">Marshennikova Olga</h1>
 
         <Section title="PROFESSIONAL EXPERIENCE">
           {experience.map((item, index) => (
-            <ExperienceItem key={index} item={item} />
+            <ExperienceItem
+              key={index}
+              item={{
+                title: item.title,
+                description: item.description?.join(" "),
+                link: item.link,
+              }}
+            />
           ))}
         </Section>
 
@@ -189,18 +187,6 @@ function RouteComponent() {
         </Section>
 
         <SkillsSection />
-
-        <div className="flex flex-row justify-center gap-6 mt-8">
-          {links.map((link, index) => (
-            <a
-              key={index}
-              href={link.href}
-              className="text-xl hover:text-purple-500 transition-colors duration-300"
-            >
-              <FontAwesomeIcon icon={link.icon} />
-            </a>
-          ))}
-        </div>
       </div>
     </div>
   )
--- web/app/routes/index.tsx
+++ web/app/routes/index.tsx
@@ -1,26 +1,60 @@
 import { createFileRoute } from "@tanstack/react-router"
 import { Link } from "@tanstack/react-router"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faInstagram,
+  faGithub,
+  faXTwitter,
+  faSpotify,
+  faDiscord,
+  faGoogle,
+} from "@fortawesome/free-brands-svg-icons"
 
 function CVComponent() {
+  const links = [
+    { icon: faGithub, href: "https://github.com/olyamrshn" },
+    { icon: faXTwitter, href: "https://twitter.com/leatbabies" },
+    { icon: faDiscord, href: "https://discord.com/users/704485765010161966" },
+    { icon: faGoogle, href: "mailto:olyamrshn@gmail.com" },
+    {
+      icon: faSpotify,
+      href: "https://open.spotify.com/user/awpkpo5yx2t80iqa4tgfvp7a7",
+    },
+    { icon: faInstagram, href: "https://www.instagram.com/olyamrshn" },
+  ]
+
   return (
-    <div className="p-8 flex flex-col items-center justify-center text-white">
-      <p className="text-lg mb-8 max-w-2xl text-start">
-        Hey, I'm a frontend developer based in Madrid. I work on{" "}
+    <div className="min-h-screen p-8 flex flex-col max-w-xl mx-auto text-left items-start justify-center text-white space-y-2">
+      <p>Hi there! I'm a frontend developer based in Madrid.</p>
+      <p>
+        {" "}
+        Enjoy contributing to{" "}
         <Link
           to="/cv"
           className="text-white opacity-70 hover:opacity-90 transition-opacity duration-300 cursor-pointer"
         >
           open source projects
         </Link>{" "}
-        and write{" "}
+        and keeping{" "}
         <Link
           to="/diary"
           className="text-white opacity-70 hover:opacity-90 transition-opacity duration-300 cursor-pointer"
         >
-          notes in my diary
+          a dev & live journal
         </Link>{" "}
-        in my free time.
+        in my spare time.
       </p>
+      <div className="flex flex-row gap-6 mt-8 self-start">
+        {links.map((link, index) => (
+          <a
+            key={index}
+            href={link.href}
+            className="text-xl text-white opacity-70 hover:opacity-90 transition-opacity duration-300"
+          >
+            <FontAwesomeIcon icon={link.icon} />
+          </a>
+        ))}
+      </div>
     </div>
   )
 }
